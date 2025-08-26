46028ec6ffb69ccb0571e83c6572091320ebacb4
nullhq <taboutsafredy@gmail.com>
2025-08-26 12:12:13 +0100

    learn: done with the timestamp project ;)

--- backend-dev-and-apis/project-timestamp/index.js
+++ backend-dev-and-apis/project-timestamp/index.js
@@ -18,13 +18,19 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date;
+  if(req.params.date) {
+    date = new Date(isNaN(Number(req.params.date)) ? req.params.date : Number(req.params.date));
+    if (isNaN(date.valueOf())) {
+      return res.json({error : "Invalid Date"});
+    }
+    res.json({unix: date.valueOf(), utc: date.toString()});
+  } else {
+    date = new Date();
+    res.json({unix: date.valueOf(), utc: date.toString()});
+  }
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
